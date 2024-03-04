ae7687d31c7430195c359923389e49a1da696b1e
Yuniko Satria Ivantara <147712399+yunikosi@users.noreply.github.com>
2024-03-04 12:49:31 +0700

    Add files via upload

---
+++ pertemuan1.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo"<font size='18' color = 'red' style='font-family:tahoma;'><center><b>BIODATA</b></center></font>
+            <br><font size='12' color = 'blue' style='font-family:tahoma; margin-left:42rem;'>Nama : <b>Achmad Riki</b></font><br>
+            <font size='12' color = 'blue' style='font-family:tahoma; margin-left:42rem;'>Email : ariki@yahoo.com</font><br>
+            <font size='12' color = 'blue' style='font-family:tahoma; margin-left:42rem;'>Jurusan : Teknik Informatika</font><br>
+            <font size='12' color = 'blue' style='font-family:tahoma; margin-left:42rem;'>Hoby : Travelling</font><br>
+    ";
+
+?>

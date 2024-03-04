3409624a31eceae85fec3b412c05d593477e3860
Yuniko Satria Ivantara <147712399+yunikosi@users.noreply.github.com>
2024-03-04 12:58:29 +0700

    Update pertemuan1.php

--- pertemuan1.php
+++ pertemuan1.php
@@ -7,4 +7,7 @@
             <font size='12' color = 'blue' style='font-family:tahoma; margin-left:42rem;'>Hoby : Travelling</font><br>
     ";
 
+    echo"<br><br><br><font size='15' color='green'><center><b>KESAN BELAJAR PHP</b></center></font><br>
+            <br><font size='12'><center>Kesan saya adalah PHP tidak terlalu baku bahasanya dan lebih mudah dipahami</center></font>";
+
 ?>

d6f09ff83cdb5a6bc923ef24c75353d8784cb8f2
Yuniko Satria Ivantara <147712399+yunikosi@users.noreply.github.com>
2024-03-08 20:29:40 +0700

    Add files via upload

---
+++ Pertemuan3/latihan1.php
@@ -0,0 +1,23 @@
+<?php
+$ball = 'purple';
+// Coba ubah ke "yellow", "blue", "green", "purple", atau warna lain
+    if ($ball == 'red') {
+        $redbox = $ball;
+        echo "redbox: $redbox<br>\n";
+    } elseif ($ball == 'yellow') {
+        $yellowbox = $ball;
+        echo "yellowbox: $yellowbox<br>\n";
+    } elseif ($ball == 'blue') {
+        $bluebox = $ball;
+        echo "bluebox: $bluebox<br>\n";
+    } elseif ($ball == 'green') {
+        $greenbox = $ball;
+        echo "greenbox: $greenbox<br>\n";
+    } elseif ($ball == 'purple') {
+        $purplebox = $ball;
+        echo "purplebox: $purplebox<br>\n";
+    } else {
+        $colorlessbox = $ball;
+        echo "colorlessbox: $colorlessbox<br>\n";
+    }
+?>

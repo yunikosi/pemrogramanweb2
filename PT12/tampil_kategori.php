1b769914f348311af09fdbcd5e8feffe5bd9fa8a
Yuniko Satria Ivantara <147712399+yunikosi@users.noreply.github.com>
2024-05-17 01:29:29 +0700

    Update tampil_kategori.php

--- PT12/tampil_kategori.php
+++ PT12/tampil_kategori.php
@@ -8,7 +8,7 @@
 
     echo"
     
-    <div class = 'container-fluid text-white mt-5 mx-auto'>
+    <div class = 'container-fluid text-white mt-5 mx-auto pb-5'>
             
         <div class='content px-3 py-3 bg-primary border rounded shadow-sm'>
             <div class= 'judul mb-4'>

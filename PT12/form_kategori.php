7f732b82b6b66a022e95f6b36725d5baa1ae6f92
Yuniko Satria Ivantara <147712399+yunikosi@users.noreply.github.com>
2024-05-17 20:27:05 +0700

    Update form_kategori.php

--- PT12/form_kategori.php
+++ PT12/form_kategori.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Form Input</title>
+    <title>Kategori dan Berita</title>
 </head>
 <body class="bg-info">
     <div class="container-fluid">
@@ -34,9 +34,13 @@
     <?php 
     
         include"tampil_kategori.php";
+        include"form_berita.php";
+        include"tampil_berita.php";
+        include"tampil.php";
 
     ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
